ac498da09300db7fb18255d6c61bd9c69808eea4
AlfredEschbaum <alfred.eschbaum@khk.ee>
2021-11-25 16:32:57 +0200

    praks1/5.4 jukebox.py

--- praks1/5.4 jukebox.py
+++ praks1/5.4 jukebox.py
@@ -5,10 +5,11 @@ loend = []
 mitmespala = 1
 for rida in fail:
     print(str(mitmespala) + str(rida[:-1]))
-
-
-
-
-
+    mitmespala += 1
+    loend.append(rida)
+lauluvalimine = int(input("Valige laulu järjekorranumber : "))
+lauluvalimine -= 1
+print("Mängitav pala on " + loend[lauluvalimine])
+    
 
 fail.close()
